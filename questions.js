5d58b7abe8c03c5e54e09f38a64f33d6a09ee0ea
jack upt <jhonbahama@gmail.com>
2019-12-06 23:40:24 -0800

    Quiz Updated

---
+++ questions.js
@@ -0,0 +1,25 @@
+
+// var questions =[    
+//     {   
+//         title:"Commonly used data types DO NOT inclute:",
+//         Choices:["strings","booleans","alerts","numbers"],
+//         answer:"alerts"
+//     },
+//     {   
+//         title:"The condition in an if/else statement is enclosed within _____.",
+//         Choices:["quotes","curly brackets","parentheses","square brackets"],
+//         answer:"parentheses"
+//     },
+//     {   tittle:"Arrays in JavaScript can be used to store______.",
+//          Choices:["numbers and strings","other arrays","boolens ","all of the above"],
+//         answer:"all of the above"
+//     },
+//     {   
+//         title:"String values must be enclosed within_____when being assigned to variables.",
+//         Choices:["commas","curly backets","quotes","parentheses"],
+//         answer:"quotes"
+//     },
+    
+    
+    
+    // ]
---
+++ script.js
@@ -0,0 +1,103 @@
+var question = document.getElementById("question");
+var choices =Array.from (document.getElementsByClassName("choice-text"));
+var currentQuestion={};
+var score= 0;
+var questionCounter= 0;
+var availableQuestions= [];
+var timerEl = document.querySelector(".timer")
+var secondsLeft= 75;
+
+let questions =[    
+    {   
+        question:"Commonly used data types DO NOT include:",
+        choice1:"strings",
+        choice2:"booleans",
+        choice3:"alerts",
+        choice4:"numbers",
+        answer: 3
+    },
+    {   
+       question:"The condition in an if/else statement is enclosed within _____.",
+        choice1:"quotes",
+        choice2:"curly brackets",
+        choice3:"parentheses",
+        choice4:"square brackets",
+        answer:3
+    },
+    {   question:"Arrays in JavaScript can be used to store______.",
+         choice1:"numbers and strings",
+         choice2:"other arrays",
+         choice3:"boolens",
+         choice4:"all of the above",
+        answer:4
+    },
+    {   
+        question:"String values must be enclosed within_____when being assigned to variables.",
+        choice1:"commas",
+        choice2:"curly backets",
+        choice3:"quotes",
+        choice4:"parentheses",
+        answer:3
+    }
+    
+     ];
+
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS =5;
+
+function startQuiz() {
+    questionCounter= 0;
+    score= 0;
+    availableQuestions= [...questions];
+console.log(availableQuestions);
+getNewQuestion();
+};
+
+function getNewQuestion (){ 
+    if (availableQuestions.length===0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('viewscore.html');
+    }
+  
+    questionCounter++;
+       const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+       currentQuestion =availableQuestions[questionIndex];
+       question.innerText= currentQuestion.question;
+       
+       choices.forEach(choice => {
+           const number = choice.dataset["number"];
+           choice.innerText = currentQuestion ["choice" + number];
+           
+       });
+       availableQuestions.splice(questionIndex,1);
+       acceptingAnswers= true;
+   };
+choices.forEach(choice => {
+    choice.addEventListener('click', e =>  {
+       if(!acceptingAnswers) return;
+
+       acceptingAnswers = false;
+       const selectedChoice = e.target;
+       const selectedAnswer = selectedChoice.dataset["number"];
+       getNewQuestion();
+   
+});
+    
+});
+    
+   startQuiz();
+
+function setTime() {
+      var timerInterval = setInterval(function(startQuiz) {
+        secondsLeft--;
+        timerEl.textContent ="Time:" + secondsLeft ;
+    
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          sendMessage();
+        }
+    
+      }, 1000);
+    }function sendMessage() {
+      timerEl.textContent = "Time is Up! ";
+    }
+    setTime();
